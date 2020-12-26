e021b1738cc67fc1158983836ac346b7728639d8
Ljj <2487261594@qq.com>
2020-12-26 18:47:51 +0800

    20201226

--- inherit_demo/front/views.py
+++ inherit_demo/front/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 def index(request):
-    return render(request,'index.html')
-def company(requests):
-    pass
+    context = {'username': 'Star'}
+    return render(request,'index.html',context=context)
+def company(request):
+    return render(request,'company.html')
 def school(request):
-    pass
+    return render(request,'school.html')
 # Create your views here.
